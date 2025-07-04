4d5e02774632aa461aa2ca5e3a4ebf2a15f57836
Kristian Repo <krepo@c1r3p7.hive.fi>
2025-07-04 16:30:06 +0300

    Updated changes being committed

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: krepo <krepo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 09:08:45 by krepo             #+#    #+#             */
-/*   Updated: 2025/07/03 14:15:38 by krepo            ###   ########.fr       */
+/*   Updated: 2025/07/04 14:54:58 by krepo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static char	**args_and_init_stack(int ac, char *av[], t_stack_node **a);
+static bool	initial_error_check(int ac, char *av[]);
 
 int	main(int ac, char *av[])
 {
@@ -22,10 +23,9 @@ int	main(int ac, char *av[])
 
 	a = NULL;
 	b = NULL;
-	split_av = NULL;
-	if (ac == 1 || (ac == 2 && av[1][0] == '\0'))
-		return (1);
 	split_av = args_and_init_stack(ac, av, &a);
+	if (!a)
+		return (1);
 	if (!stack_sorted(a))
 	{
 		if (stack_len(a) == 2)
@@ -47,6 +47,8 @@ static char	**args_and_init_stack(int ac, char *av[], t_stack_node **a)
 	char	**split_av;
 
 	split_av = NULL;
+	if (initial_error_check(ac, av))
+		return (NULL);
 	if (ac == 2)
 	{
 		split_av = ft_split(av[1], ' ');
@@ -59,5 +61,22 @@ static char	**args_and_init_stack(int ac, char *av[], t_stack_node **a)
 	}
 	else
 		stack_init_a(a, av + 1);
+	if (!*a && (ac > 1 && !(ac == 2 && !av[1][0])))
+	{
+		if (split_av)
+			free_matrix(split_av);
+		write(2, "Error\n", 6);
+		exit(1);
+	}
 	return (split_av);
+}
+
+static bool	initial_error_check(int ac, char	*av[])
+{
+	if (ac == 1 || (ac == 2 && av[1][0] == '\0'))
+	{
+		write(2, "Error\n", 6);
+		return (true);
+	}
+	return (false);
 }
--- sort_algo.c
+++ sort_algo.c
@@ -6,7 +6,7 @@
 /*   By: krepo <krepo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 11:04:18 by krepo             #+#    #+#             */
-/*   Updated: 2025/07/03 13:25:05 by krepo            ###   ########.fr       */
+/*   Updated: 2025/07/04 10:44:24 by krepo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,15 @@ static void	min_on_top(t_stack_node **a);
 
 void	sort_stacks(t_stack_node **a, t_stack_node **b)
 {
-	int	len_a;
-
-	len_a = stack_len(*a);
-	while (len_a-- > 3 && !stack_sorted(*a))
+	pb(b, a, false);
+	pb(b, a, false);
+	while (stack_len(*a) > 3 && !stack_sorted(*a))
 	{
 		init_nodes_a(*a, *b);
 		move_a_to_b(a, b);
 	}
 	sort_three(a);
-	while (b)
+	while (*b)
 	{
 		init_nodes_b(*a, *b);
 		move_b_to_a(a, b);
@@ -41,25 +40,35 @@ static void	move_a_to_b(t_stack_node **a, t_stack_node **b)
 	t_stack_node	*cheapest_node;
 
 	cheapest_node = get_cheapest(*a);
+	if (!cheapest_node)
+		return ;
 	if (cheapest_node->above_median && cheapest_node->target_node->above_median)
 		rotate_both(a, b, cheapest_node);
 	else if (!(cheapest_node->above_median)
 		&& !(cheapest_node->target_node->above_median))
 		rev_rotate_both(a, b, cheapest_node);
 	prep_for_push(a, cheapest_node, 'a');
-	prep_for_push(b, cheapest_node, 'b');
+	prep_for_push(b, cheapest_node->target_node, 'b');
 	pb(b, a, false);
 }
 
 static void	move_b_to_a(t_stack_node **a, t_stack_node **b)
 {
-	prep_for_push(a, (*b)->target_node, 'a');
-	pa(a, b, false);
+	if (*b && (*b)->target_node)
+	{
+		prep_for_push(a, (*b)->target_node, 'a');
+		pa(a, b, false);
+	}
 }
 
 static void	min_on_top(t_stack_node **a)
 {
-	while ((*a)->value != find_min(*a)->value)
+	t_stack_node	*min_node;
+
+	min_node = find_min(*a);
+	if (!min_node)
+		return ;
+	while ((*a)->value != min_node->value)
 	{
 		if (find_min(*a)->above_median)
 			ra(a, false);
